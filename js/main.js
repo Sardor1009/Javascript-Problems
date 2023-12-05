9a2cab7ba99615ffd1dc338b739351f6e30a91ba
Sardor1009 <islomovsardor1009@gmail.com>
2023-12-05 10:39:46 +0500

    some

---
+++ js/main.js
@@ -0,0 +1,267 @@
+console.log(12 * 12)
+console.log("abc" / 4)
+
+// Boolean 1
+
+let A = +prompt('A ning qiymatini kiriting');
+ 
+let res = A % 2 == 0;
+
+console.log(res);
+
+// Boolean 2
+
+// let A = +prompt('A ning qiymatini kiriting');
+// let B= +prompt('B ning qiymatini kiriting');
+
+
+// let res = A >= 0 || B< -2;
+
+// console.log(res);
+
+// Boolean 3
+
+// let A = +prompt('A ning qiymatini kiriting');
+// let B= +prompt('B ning qiymatini kiriting');
+// let C= +prompt('C ning qiymatini kiriting');
+
+// let res = A < B && B < C;
+
+// console.log(res);
+
+// Boolean 4
+
+// let A = +prompt('A ning qiymatini kiriting');
+// let B= +prompt('B ning qiymatini kiriting');
+
+// let res = ((A % 2 == 1) && (B % 2 == 1));
+
+// console.log(res);
+
+// Boolean 5
+
+// let A = +prompt('A ning qiymatini kiriting');
+// let B= +prompt('B ning qiymatini kiriting');
+
+// let res = ((A + B) % 2 == 1) && (A % 2 == 1) && (B % 2 == 1) ;
+
+// console.log(res);
+
+// Boolean 6
+
+// let A = +prompt('A ning qiymatini kiriting');
+// let B= +prompt('B ning qiymatini kiriting');
+
+// let res = (A + B) % 2 == 1;
+
+// console.log(res);
+
+// Boolean 7
+
+// let A = +prompt('A ning qiymatini kiriting');
+// let B= +prompt('B ning qiymatini kiriting');
+// let C= +prompt('C ning qiymatini kiriting');
+
+// let res = A > 0 && B > 0 && C > 0;
+
+// console.log(res);
+
+// Boolean 8
+
+// let A = +prompt('A ning qiymatini kiriting');
+// let B= +prompt('B ning qiymatini kiriting');
+// let C= +prompt('C ning qiymatini kiriting');
+
+// let res = A > 0 && B < 0 && C < 0  || B > 0 && A < 0 && C < 0 || C > 0 && A < 0 && B < 0;
+
+// console.log(res);
+
+
+// Boolean 9
+
+// let A = +prompt('A ning qiymatini kiriting');
+// let B= +prompt('B ning qiymatini kiriting');
+// let C= +prompt('C ning qiymatini kiriting');
+
+// let res = A > 0 && B > 0 && C < 0  || B > 0 && A > 0 && C < 0 || C > 0 && A > 0 && B < 0;
+
+// console.log(res);
+
+// Boolean 10
+
+// let ab = +prompt('ab ning qiymatini kiriting');
+
+
+// let res = (ab >= 10  &&   ab < 100 )  && (ab % 2 == 0);
+
+// console.log(res);
+
+
+
+// Boolean 11
+
+// let abc = +prompt('abc ning qiymatini kiriting');
+
+// let res = (abc >= 100  &&   ab < 1000 )  && (abc % 2 == 0);
+
+// console.log(res);
+
+
+
+// Boolean 12
+
+// let a = +prompt('a ning qiymatini kiriting');
+// let b = +prompt('b ning qiymatini kiriting');
+// let c = +prompt('c ning qiymatini kiriting');
+
+// let res = ((a == b * (-1)) || (b == c * (-1)) || (c == a * (-1)));
+
+// console.log(res);
+
+
+// Boolean 13
+
+// let abc = +prompt('abc ning qiymatini kiriting');
+
+// let c = abc % 10;
+// let b = ((abc-c) /10) % 10;
+// let a = Math.trunc(abc/100);
+
+// let res = a < b && b < c;
+
+// console.log(res);
+
+
+// Boolean 14
+
+// let abc = +prompt('abc ning qiymatini kiriting');
+
+// let c = abc % 10;
+// let b = ((abc-c) /10) % 10;
+// let a = Math.trunc(abc/100);
+
+// let res = (a < b && b < c)  ||  (a > b && b > c);
+
+// console.log(res);
+
+// Boolean 15
+
+// let abc = +prompt('abc ning qiymatini kiriting');
+
+// let c = abc % 10;
+// let b = ((abc-c) /10) % 10;
+// let a = Math.trunc(abc/100);
+
+// let res = (a==c);
+
+// console.log(res);
+
+
+
+// MISOL 1
+
+// let A = +prompt('A ning qiymatini kiriting');
+// let B = +prompt('B ning qiymatini kiriting');
+
+
+// let Y = - (B/2)*(Math.pow(A,2) + Math.pow(B, 2));
+
+// console.log(Y);
+
+// MISOL 2
+
+// let A = +prompt('A ning qiymatini kiriting');
+// let B = +prompt('B ning qiymatini kiriting');
+// let C = +prompt('C  ning qiymatini kiriting');
+// let D = +prompt('D  ning qiymatini kiriting');
+
+// let Y = Math.pow((( A + B )/ ( C + D )),2) + Math.pow(B, 2);
+
+// console.log(Y);
+
+// MISOL 3
+
+// let A = +prompt('A ning qiymatini kiriting');
+// let B = +prompt('B ning qiymatini kiriting');
+// let C = +prompt('C  ning qiymatini kiriting');
+// let D = +prompt('D  ning qiymatini kiriting');
+
+// let Y = 2 * (Math.pow(A, D) + (4 * Math.pow(C, 2)) / 3 );
+
+// console.log(Y);
+
+// MISOL 4
+
+// let A = +prompt('A ning qiymatini kiriting');
+// let B = +prompt('B ning qiymatini kiriting');
+// let C = +prompt('C  ning qiymatini kiriting');
+// let D = +prompt('D  ning qiymatini kiriting');
+
+// let Y = 1 / Math.pow(A, 2) * Math.pow((B/10), 3) * Math.pow((C + D), 2);
+
+// console.log(Y);
+
+// MISOL 5
+
+// let A = +prompt('A ning qiymatini kiriting');
+// let B = +prompt('B ning qiymatini kiriting');
+// let C = +prompt('C  ning qiymatini kiriting');
+// let D = +prompt('D  ning qiymatini kiriting');
+
+// let Y = Math.pow( ( (A/B-1) / (C / (D-1))) ,  2);
+
+// console.log(Y);
+
+// MISOL 6
+
+// let A = +prompt('A ning qiymatini kiriting');
+// let B = +prompt('B ning qiymatini kiriting');
+// let C = +prompt('C  ning qiymatini kiriting');
+// let D = +prompt('D  ning qiymatini kiriting');
+
+// let Y = (A + B) / (C + D /(A + C));
+
+// console.log(Y);
+
+// MISOL 7
+
+// let A = +prompt('A ning qiymatini kiriting');
+// let B = +prompt('B ning qiymatini kiriting');
+// let C = +prompt('C  ning qiymatini kiriting');
+
+// let Y = Math.sqrt(Math.pow((A+B+C), 2) + Math.pow((A-B-C), 2));
+
+// console.log(Y);
+
+// MISOL 8
+
+// let A = +prompt('A ning qiymatini kiriting');
+// let B = +prompt('B ning qiymatini kiriting');
+// let C = +prompt('C  ning qiymatini kiriting');
+
+// let Y = (Math.pow(A, 2) + Math.pow(B, 2) + Math.pow(C, 2) ) / (A * B *C);
+
+// console.log(Y);
+
+// MISOL 9
+
+// let A = +prompt('A ning qiymatini kiriting');
+// let B = +prompt('B ning qiymatini kiriting');
+// let C = +prompt('C  ning qiymatini kiriting');
+// let D = +prompt('D  ning qiymatini kiriting');
+
+// let Y = A * Math.pow(D, 3) / 3 + B * Math.pow(C, 2) / 2;
+
+// console.log(Y);
+
+// MISOL 10
+
+// let A = +prompt('A ning qiymatini kiriting');
+// let B = +prompt('B ning qiymatini kiriting');
+// let C = +prompt('C  ning qiymatini kiriting');
+// let D = +prompt('D  ning qiymatini kiriting');
+
+
+// let Y = Math.abs(A/B - B/C) + Math.pow((A + C), (1/4));
+
+// console.log(Y);
--- main.js
+++
@@ -1,2 +0,0 @@
-console.log(12 * 12)
-console.log("abc" / 4)
